49b04f2a3640805ec8fb40f9f671a954ff45faa5
Andrea Parodi <andrea@parro.it>
2016-01-15 17:47:28 +0100

    switched tests to tape

--- test/folderify_test.js
+++ test/folderify_test.js
@@ -2,6 +2,7 @@
 
 var expect = require('expect.js');
 var concat = require('concat-stream');
+var test = require('tape');
 var fs = require('fs');
 var path = require('path');
 var folderify = require('../lib/folderify');
@@ -11,12 +12,12 @@ function rf (file) {
   return fs.readFileSync(path.join(__dirname, file), 'utf8');
 }
 
-function checkTransform (sourcefile, expectedfile, done) {
+function checkTransform (sourcefile, expectedfile, t) {
   var source = rf(sourcefile);
   var expected = rf(expectedfile);
   var result = concat(function (data) {
     expect(data).to.be.equal(expected);
-    done();
+    t.end();
   });
   var stream = folderify(sourcefile);
   stream.pipe(result);
@@ -24,125 +25,126 @@ function checkTransform (sourcefile, expectedfile, done) {
   stream.end();
 }
 
-describe('folderify', function () {
-  it('is defined', function () {
-    expect(folderify).to.be.an('function');
+test('folderify exports a function', function (t) {
+  expect(folderify).to.be.an('function');
+  t.end();
+});
+
+test('return a through stream', function (t) {
+  expect(folderify().constructor.name).to.be.equal('Stream');
+  t.end();
+});
+
+test('un-requires include-folder', function (t) {
+  checkTransform(
+    'fixtures/source/unrequire-include-folder.js',
+    'fixtures/expected/unrequire-include-folder.js',
+    t
+  );
+});
+
+test('replaces includeFolder call with `files` array', function (t) {
+  checkTransform(
+    'fixtures/source/include-folder-default.js',
+    'fixtures/expected/include-folder-default.js',
+    t
+  );
+});
+
+test('respects includeFolder pattern-match arguments', function (t) {
+  checkTransform(
+    'fixtures/source/include-folder-regex.js',
+    'fixtures/expected/include-folder-regex.js',
+    t
+  );
+});
+
+test('preserves filenames when option is set', function (t) {
+  checkTransform(
+    'fixtures/source/include-folder-filenames.js',
+    'fixtures/expected/include-folder-filenames.js',
+    t
+  );
+});
+
+test('as a browserify transform', function (s) {
+  var expectedBundle = rf('fixtures/expected/bundle.js');
+  var expectedBundleWithJson = rf('fixtures/expected/bundle-with-json.js');
+
+  s.test('doesn\'t require brfs', function (t) {
+    var b = browserify(path.join(__dirname, 'fixtures/source/bundle.js'));
+    b.transform(folderify);
+
+    b
+      .bundle()
+      .on('error', t.end.bind(t))
+      .pipe(concat(function (data) {
+        expect(data.toString('utf8')).to.be.equal(expectedBundle);
+        t.end();
+      }));
   });
 
-  it('return a through stream', function () {
-    expect(folderify().constructor.name).to.be.equal('Stream');
+  s.test('supports brfs running after folderify', function (t) {
+    var b = browserify(path.join(__dirname, 'fixtures/source/bundle.js'));
+    b.transform(folderify);
+    b.transform('brfs');
+
+    b
+      .bundle()
+      .on('error', t.end.bind(t))
+      .pipe(concat(function (data) {
+        expect(data.toString('utf8')).to.be.equal(expectedBundle);
+        t.end();
+      }));
   });
 
-  this.timeout(3000);
+  s.test('support brfs running before folderify', function (t) {
+    var b = browserify(__dirname + '/fixtures/source/bundle.js');
+    b.transform('brfs');
+    b.transform(folderify);
 
-  it('un-requires include-folder', function (done) {
+    b
+      .bundle()
+      .on('error', t.end.bind(t))
+      .pipe(concat(function (data) {
+        expect(data.toString('utf8')).to.be.equal(expectedBundle);
+        t.end();
+      }));
+  });
+
+  s.test('support bundles with non JavaScript files', function (t) {
+    var b = browserify(__dirname + '/fixtures/source/bundle-with-json.js');
+    b.transform('brfs');
+    b.transform(folderify);
+
+    b
+      .bundle()
+      .on('error', t.end.bind(t))
+      .pipe(concat(function (data) {
+        expect(data.toString('utf8')).to.be.equal(expectedBundleWithJson);
+        t.end();
+      }));
+  });
+});
+
+test('Custom extensions', function (s) {
+  s.test('expose validExtensions', function (t) {
+    expect(folderify.validExtensions).to.be.an('array');
+    t.end();
+  });
+
+  s.test('can modify validExtensions for allowing custom extensions', function (t) {
+    var origin = folderify.validExtensions;
+    folderify.validExtensions = ['.custom-js'];
     checkTransform(
-      'fixtures/source/unrequire-include-folder.js',
-      'fixtures/expected/unrequire-include-folder.js',
-      done
-    );
-  });
-
-  it('replaces includeFolder call with `files` array', function (done) {
-    checkTransform(
-      'fixtures/source/include-folder-default.js',
-      'fixtures/expected/include-folder-default.js',
-      done
-    );
-  });
-
-  it('respects includeFolder pattern-match arguments', function (done) {
-    checkTransform(
-      'fixtures/source/include-folder-regex.js',
-      'fixtures/expected/include-folder-regex.js',
-      done
-    );
-  });
-
-  it('preserves filenames when option is set', function (done) {
-    checkTransform(
-      'fixtures/source/include-folder-filenames.js',
-      'fixtures/expected/include-folder-filenames.js',
-      done
-    );
-  });
-
-  describe('as a browserify transform', function () {
-    var expectedBundle = rf('fixtures/expected/bundle.js');
-    var expectedBundleWithJson = rf('fixtures/expected/bundle-with-json.js');
-
-    it('doesn\'t require brfs', function (done) {
-      var b = browserify(path.join(__dirname, 'fixtures/source/bundle.js'));
-      b.transform(folderify);
-
-      b
-        .bundle()
-        .on('error', done)
-        .pipe(concat(function (data) {
-          expect(data.toString('utf8')).to.be.equal(expectedBundle);
-          done();
-        }));
-    });
-
-    it('supports brfs running after folderify', function (done) {
-      var b = browserify(path.join(__dirname, 'fixtures/source/bundle.js'));
-      b.transform(folderify);
-      b.transform('brfs');
-
-      b
-        .bundle()
-        .on('error', done)
-        .pipe(concat(function (data) {
-          expect(data.toString('utf8')).to.be.equal(expectedBundle);
-          done();
-        }));
-    });
-
-    it('support brfs running before folderify', function (done) {
-      var b = browserify(__dirname + '/fixtures/source/bundle.js');
-      b.transform('brfs');
-      b.transform(folderify);
-
-      b
-        .bundle()
-        .on('error', done)
-        .pipe(concat(function (data) {
-          expect(data.toString('utf8')).to.be.equal(expectedBundle);
-          done();
-        }));
-    });
-
-    it('support bundles with non JavaScript files', function (done) {
-      var b = browserify(__dirname + '/fixtures/source/bundle-with-json.js');
-      b.transform('brfs');
-      b.transform(folderify);
-
-      b
-        .bundle()
-        .on('error', done)
-        .pipe(concat(function (data) {
-          expect(data.toString('utf8')).to.be.equal(expectedBundleWithJson);
-          done();
-        }));
-    });
-  });
-
-  describe('Custom extensions', function () {
-    it('expose validExtensions', function () {
-      expect(folderify.validExtensions).to.be.an('array');
-    });
-
-    it('can modify validExtensions for allowing custom extensions', function (done) {
-      var origin = folderify.validExtensions;
-      folderify.validExtensions = ['.custom-js'];
-      checkTransform(
-        'fixtures/source/include-folder-default.custom-js',
-        'fixtures/expected/include-folder-default.custom-js',
-        function (err) {
+      'fixtures/source/include-folder-default.custom-js',
+      'fixtures/expected/include-folder-default.custom-js',
+      {
+        end: function (err) {
           folderify.validExtensions = origin;
-          done(err);
+          t.end(err);
         }
-      );
-    });
+      }
+    );
   });
 });
